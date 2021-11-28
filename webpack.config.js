0312b420c12fcf8d5dc8089231ed3fd661404181
iamanas20 <anaslatique@gmail.com>
2021-11-28 16:27:28 +0100

    add favicon

--- webpack.config.js
+++ webpack.config.js
@@ -77,6 +77,7 @@ module.exports = {
   plugins: [
   	new HtmlWebpackPlugin({
       template: path.join(__dirname, "/index.html"),
+      favicon: "./favicon.png"
     }),
 		new MiniCssExtractPlugin(),
   ]
